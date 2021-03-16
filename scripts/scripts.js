9690c166b5bb1ce93dbcd75eed8bed8c780c75da
Marckul <marcin27kula@gmail.com>
2021-03-16 10:34:30 +0100

    news page has been added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,45 +6,84 @@ $('.carousel').carousel({
 
 
 // Position  checking
-const offsetVal = 250;
+const offsetVal = 400;
 $('body').scrollspy(
-  { target: '#navbar-example',  offset: offsetVal }
+    { target: '#navbar-example', offset: offsetVal }
 )
 
+// window.onscroll = function() {
+//     console.log("stop");
+//     win = $(window);
+//     console.log(win.scrollTop())
+// }
 
+const shift = 100;
 // https://www.codeply.com/go/Pv5Ef2WbsR/bootstrap-smooth-scroll-template
 // $("nav ul li a[href^='#']").on('click', function (e) {
 /* animate smooth scrolling sections */
-$("a[href^='#']").on('click', function (e) {
-
+$(".navbar a[href^='#'], a[href^='#'].smooth-scrolling, .smooth-scrolling").on('click', function (event) {
+    console.log("im here");
     // prevent default anchor click behavior
-    e.preventDefault();
-
+    event.preventDefault();
+    win = $(window);
     // store hash
     let hash = this.hash;
 
     /* 
-    console.log("hash")
-    console.log(hash);
     console.log('$(hash).offset()');
     console.log(typeof $(hash).offset() === "undefined");
-    console.log($(hash).offset()); */
+    */
+    console.log(`hash: ${hash}`);
+    console.log(`$(window).offset(): ${win.scrollTop()}`);
+
+    win.scrollTop();
+    
+
 
     if (hash === "") {
         console.warn("This is empty link");
+        return null;
+    } 
+    
+    console.log(`$(hash).offset().top  ${ $(hash).offset().top }`);
+    let destPoint = $(hash).offset().top - shift;
+    console.log(`destPoint: ${destPoint}`)
+         
+    if (destPoint < 0) {
+        destPoint = 0;
+    }
 
-    } else if (typeof $(hash).offset() === "undefined") {
+    if (typeof $(hash).offset() === "undefined") {
         console.warn("This link not provide to any element on this page");
+
+    } else if ( destPoint < 0 && window.location.hash === hash ) {
+        console.log("Destination point beyond window");
+        console.log(`window location: ${win.scrollTop()}`);
+    } else if (destPoint === win.scrollTop()) {
+        console.log("currently on location");
+        if(history.pushState) {
+            history.pushState(null, null, hash);
+        }
+        else {
+            location.hash = hash;
+        }
 
     } else {
         // animate
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 300, function () {
-
+            scrollTop: destPoint
+        }, 400, function () {
+            
+            if(history.pushState) {
+                history.pushState(null, null, hash);
+            }
+            else {
+                location.hash = hash;
+            }
+            console.log($(hash).offset().top);
             // when done, add hash to url
             // (default click behaviour)
-            window.location.hash = hash;
+            // window.location.hash = hash;
             console.log("scrolling done");
         });
     }
